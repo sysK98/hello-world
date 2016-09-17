5d683f5f3ec54b73812f743a2ee6e941bc94b6f0
sysK98 <syskin98@gmail.com>
2016-09-18 00:58:01 +0900

    Add files via upload

---
+++ Datagrid.aspx
@@ -0,0 +1,156 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Datagrid.aspx.cs" Inherits="Datagrid.Datagrid" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+   <script type="text/javascript" >
+      function rowUp(idx) {
+         if (idx == 0) {
+            return;
+         }
+         i = Number(idx) - 1;     // 대상
+         k = Number(idx);    // event.this
+
+         var objUpCommonTxt = document.getElementById("rptList_txtCommonTxt_" + i);      // 대상 id
+         var objUpUseYn = document.getElementById("rptList_ddlUseYn_" + i);      // 대상 id
+         var objOrgCommonTxt = document.getElementById("rptList_txtCommonTxt_" + k);      // 원본 id
+         var objOrgUseYn = document.getElementById("rptList_ddlUseYn_" + k);      // 원본 id
+
+         var valUpCommonTxt = objUpCommonTxt.value;
+         var valUpUseYn = objUpUseYn[objUpUseYn.selectedIndex].value;
+         var valOrgCommonTxt = objOrgCommonTxt.value
+         var valOrgUseYn = objOrgUseYn[objOrgUseYn.selectedIndex].value;
+
+         objUpCommonTxt.value = valOrgCommonTxt;
+         objOrgCommonTxt.value = valUpCommonTxt;
+         objUpUseYn.value = valOrgUseYn;
+         objOrgUseYn.value = valUpUseYn;
+      }
+
+      function rowDn(idx) {
+         i = Number(idx) + 1;     // 대상
+         k = Number(idx);    // event.this
+
+         var objUpCommonTxt = document.getElementById("rptList_txtCommonTxt_" + i);      // 대상 id
+         var objUpUseYn = document.getElementById("rptList_ddlUseYn_" + i);      // 대상 id
+         var objOrgCommonTxt = document.getElementById("rptList_txtCommonTxt_" + k);      // 원본 id
+         var objOrgUseYn = document.getElementById("rptList_ddlUseYn_" + k);      // 원본 id
+
+         if (objUpCommonTxt == null){
+            return;
+         }
+
+         var valUpCommonTxt = objUpCommonTxt.value;
+         var valUpUseYn = objUpUseYn[objUpUseYn.selectedIndex].value;
+         var valOrgCommonTxt = objOrgCommonTxt.value
+         var valOrgUseYn = objOrgUseYn[objOrgUseYn.selectedIndex].value;
+
+         objUpCommonTxt.value = valOrgCommonTxt;
+         objOrgCommonTxt.value = valUpCommonTxt;
+         objUpUseYn.value = valOrgUseYn;
+         objOrgUseYn.value = valUpUseYn;
+
+      }
+
+      function valChange(idx) {
+         i = Number(idx);
+         var objFlag = document.getElementById("rptList_ltrFlag_" + i);
+         objFlag.value = "C";
+      }
+
+      function addRow() {
+         var table = document.getElementById("rptList");
+         var rowCount = table.rows.length;
+         var row = table.insertRow(rowCount);
+         // Create any cells and elements you need
+         var cell1 = row.insertCell(0);
+         var element1 = document.createElement("input");
+         element1.type = "text";
+         cell1.appendChild(element1);
+
+      }
+   </script>
+</head>
+<body>
+    <form id="form1" runat="server">
+       <table>
+          <td align="right" style="width:100px">
+            <asp:button id="btnAdd" Width="100%" runat="server" Text="AddRow" CssClass="incButton" OnClick="addNewRow"></asp:button>
+          </td>
+       </table>
+    <table cellSpacing="1" cellPadding="0" width="100%" border="0" bgColor="#CCCCCC" >
+							<asp:Repeater ID="rptList" Runat="server" OnItemDataBound="rptList_ItemDataBound" OnItemCommand="rptItemCommand" >
+								<HeaderTemplate>
+									<tr class="listTitle">
+										
+										<th style="display:none">
+											순번</th>
+										<th>
+											Text</th>
+										<th>
+											정렬순서</th>
+										<th>
+											사용여부</th>
+										<th>
+											최종입력자</th>
+										<th>
+											최종등록일</th>
+										<th>
+											삭제</th>
+										<th style="display:none">
+											우선순위(data)</th>
+										<th >
+											flag</th>
+									</tr>
+								</HeaderTemplate>
+								<ItemTemplate>
+									<tr style="BORDER-TOP: #000000 solid; BORDER-RIGHT: #000000 solid; BORDER-BOTTOM: #000000 solid; BORDER-LEFT: #000000 solid; BACKGROUND-COLOR: white">
+										<td style="display:none">
+											<asp:Literal ID="ltrSeq" Runat="server"></asp:Literal></td>
+										<td>
+											<asp:TextBox ID="txtCommonTxt" CssClass="tdTxtBox" runat="server"   ></asp:TextBox></td>
+										<td align="center">
+											<table>
+												<tr>
+													<td>
+														<asp:ImageButton ID="btnMoveUp" Runat="server" ImageUrl="~/img/up.png" Width="30" Height="20"   ></asp:ImageButton></td>
+													<td>
+														<asp:Button ID="btnMoveDn" Runat="server" Text="▽" CommandName="dn_Click"></asp:Button></td>
+												</tr>
+											</table>
+										</td>
+										<td align="center">
+											<asp:DropDownList ID="ddlUseYn" Runat="server">
+												<asp:ListItem Value="Y" Text="사용"></asp:ListItem>
+												<asp:ListItem Value="N" Text="미사용"></asp:ListItem>
+											</asp:DropDownList></td>
+										<td>
+											<asp:Literal ID="ltrUpdIdNm" Runat="server"></asp:Literal></td>
+										<td>
+											<asp:Literal ID="ltrUpdDate" Runat="server"></asp:Literal></td>
+										<td align="center">
+											<asp:Button ID="btnDel" Runat="server" Text="X" CommandName="del_Click"></asp:Button></td>
+										<td style="display:none">
+											<asp:Literal ID="ltrOrderSeq" Runat="server"></asp:Literal></td>
+										<td >
+											<asp:Literal ID="ltrFlag" Runat="server"></asp:Literal></td>
+									</tr>
+								</ItemTemplate>
+							</asp:Repeater>
+						</table>
+       <table>
+             <tr>
+                <td>
+                   <asp:Button ID="btnSave" runat="server" Text="저장" OnClick="save_Click" />
+                </td>
+                <td>
+                   <input ID="btnAddRow" type="button" name="행추가" OnClick="addRow()" />
+                </td>
+             </tr>
+          </table>
+    </form>
+</body>
+</html>
---
+++ Datagrid.aspx.cs
@@ -0,0 +1,266 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.HtmlControls;
+using System.Collections;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Datagrid
+{
+   public partial class Datagrid : System.Web.UI.Page
+   {
+      private string p1;
+
+      private int p2;
+      private string p3;
+      protected System.Web.UI.WebControls.Button btnAdd;
+
+      protected void Page_Load(object sender, EventArgs e)
+      {
+         if (!IsPostBack)
+         {
+            SqlCommand cmd = new SqlCommand("select * from csp020 order by order_seq", new SqlConnection(@"Data Source=SYSK-PC\SQLEXPRESS;Initial Catalog=DemoDB;Integrated Security=True"));
+            cmd.Connection.Open();
+            rptList.DataSource = cmd.ExecuteReader();
+            rptList.DataBind();
+            cmd.Connection.Close();
+            cmd.Connection.Dispose();
+         }
+         
+
+      }
+
+      protected void rptList_ItemDataBound(object sender, RepeaterItemEventArgs e)
+      {
+            if (e.Item.ItemType.Equals(ListItemType.Item) || e.Item.ItemType.Equals(ListItemType.AlternatingItem))
+            {
+               RepeaterItem item = e.Item as RepeaterItem;
+               //System.Data.Common.DbDataRecord Dr = (System.Data.Common.DbDataRecord)item.DataItem;
+               IDataRecord Dr = (IDataRecord)item.DataItem;
+
+               HtmlTableRow tr = item.FindControl("tr") as HtmlTableRow;
+
+               Literal ltrSeq = item.FindControl("ltrSeq") as Literal;
+               TextBox txtCommonTxt = item.FindControl("txtCommonTxt") as TextBox;
+               DropDownList ddlUseYn = (DropDownList)e.Item.FindControl("ddlUseYn");
+               Literal ltrUpdIdNm = item.FindControl("ltrUpdIdNm") as Literal;
+               Literal ltrUpdDate = item.FindControl("ltrUpdDate") as Literal;
+               Literal ltrFlag = item.FindControl("ltrFlag") as Literal;
+               ImageButton btnMoveUp = item.FindControl("btnMoveUp") as ImageButton;
+               Button btnMoveDn = item.FindControl("btnMoveDn") as Button;
+
+               ltrSeq.Text = Dr["SEQ_NO"].ToString();
+               txtCommonTxt.Text = Dr["COMMON_TXT"].ToString();
+               ddlUseYn.SelectedValue = Dr["USE_YN"].ToString();
+               ltrUpdIdNm.Text = Dr["UPD_IDNM"].ToString();
+               ltrUpdDate.Text = Dr["UPD_DATE"].ToString();
+               ltrFlag.Text = Dr["FLAG"].ToString();
+
+               //btnMoveUp.Attributes.Add("onclick", "return rowUp('" + item.ItemIndex + "', '" + txtCommonTxt.ClientID + "' , '" + ddlUseYn.ClientID + "' , '" + ltrUpdIdNm.ClientID + "' , '" + ltrUpdDate.ClientID + "' );");
+               btnMoveUp.Attributes.Add("onclick", "return rowUp('" + item.ItemIndex + "');");
+               btnMoveDn.Attributes.Add("onclick", "return rowDn('" + item.ItemIndex + "');");
+               txtCommonTxt.Attributes.Add("OnTextChanged", "return valChange('" + item.ItemIndex + "');");
+
+
+
+            }
+      }
+
+
+      protected void save_Click(object sender, EventArgs e)
+      {
+         saveData();
+      }
+
+      private void saveData()
+      {
+
+         for (int i = 0; i < rptList.Items.Count; i++)
+         {
+            TextBox txtCommonTxt = rptList.Items[i].FindControl("txtCommonTxt") as TextBox;
+            Literal ltrSeq = rptList.Items[i].FindControl("ltrSeq") as Literal;
+            DropDownList ddlUseYn = rptList.Items[i].FindControl("ddlUseYn") as DropDownList;
+
+            p1 = txtCommonTxt.Text;
+            p2 = Int32.Parse(ltrSeq.Text);
+            p3 = ddlUseYn.SelectedValue;
+
+            try
+            {
+               SqlConnection cn = new SqlConnection(@"Data Source=SYSK-PC\SQLEXPRESS;Initial Catalog=DemoDB;Integrated Security=True");
+               SqlCommand cmd = new SqlCommand("update csp020 set UPD_DATE = getdate(), COMMON_TXT = @common_txt, ORDER_SEQ = @order_seq, USE_YN = @use_yn where SEQ_NO=@seq_no", cn);
+
+               cmd.Parameters.AddWithValue("@common_txt", p1);
+               cmd.Parameters.AddWithValue("@order_seq", i);
+               cmd.Parameters.AddWithValue("@use_yn", p3);
+               cmd.Parameters.AddWithValue("@seq_no", p2);
+               cn.Open();
+               cmd.ExecuteNonQuery();
+               cn.Close();
+            }
+            catch (Exception ex)
+            {
+               throw ex;
+            }
+         }
+      }
+
+      
+      private void addRow()
+      {
+
+         DataTable dtParams = new DataTable();
+         dtParams.Columns.Add(new DataColumn("SEQ_NO", typeof(String)));
+         dtParams.Columns.Add(new DataColumn("COMMON_TXT", typeof(String)));
+         dtParams.Columns.Add(new DataColumn("USE_YN", typeof(String)));
+         dtParams.Columns.Add(new DataColumn("UPD_IDNM", typeof(String)));
+         dtParams.Columns.Add(new DataColumn("UPD_DATE", typeof(String)));
+         dtParams.Columns.Add(new DataColumn("FLAG", typeof(String)));
+         DataRow dr2 = dtParams.NewRow();
+         int seq = 0;
+         foreach (RepeaterItem item in rptList.Items)
+         {
+            DataRow dr = dtParams.NewRow();
+
+
+            Literal ltrSeq = (Literal)item.FindControl("ltrSeq");
+            dr["SEQ_NO"] = ltrSeq.Text;
+
+            TextBox txtCommonTxt = (TextBox)item.FindControl("txtCommonTxt");
+            dr["COMMON_TXT"] = txtCommonTxt.Text;
+
+            DropDownList ddlUseYn = (DropDownList)item.FindControl("ddlUseYn");
+            dr["USE_YN"] = ddlUseYn.SelectedValue;
+
+            Literal ltrUpdIdNm = (Literal)item.FindControl("ltrUpdIdNm");
+            dr["UPD_IDNM"] = ltrUpdIdNm.Text;
+
+            Literal ltrUpdDate = (Literal)item.FindControl("ltrUpdDate");
+            dr["UPD_DATE"] = ltrUpdDate.Text;
+
+            Literal ltrFlag = (Literal)item.FindControl("ltrFlag");
+            dr["FLAG"] = ltrFlag.Text;
+
+            dtParams.Rows.Add(dr);
+
+            seq++;
+
+         }
+         dr2["SEQ_NO"] = 0;
+         dr2["USE_YN"] = "Y";
+         dr2["UPD_DATE"] = null;
+         dtParams.Rows.Add(dr2);
+         rptList.DataSource = dtParams;
+         rptList.DataBind();
+      }
+
+
+
+
+
+      private void bind_dataGrid()
+      {
+         DataTable dtParams = new DataTable();
+         dtParams.Columns.Add(new DataColumn("SEQ_NO", typeof(String)));
+         dtParams.Columns.Add(new DataColumn("COMMON_TXT", typeof(String)));
+         dtParams.Columns.Add(new DataColumn("USE_YN", typeof(String)));
+         dtParams.Columns.Add(new DataColumn("UPD_IDNM", typeof(String)));
+         dtParams.Columns.Add(new DataColumn("UPD_DATE", typeof(String)));
+         dtParams.Columns.Add(new DataColumn("FLAG", typeof(String)));
+
+         DataRow dr1 = dtParams.NewRow();
+
+         dr1["SEQ_NO"] = "11";
+         dr1["COMMON_TXT"] = "22";
+         dr1["USE_YN"] = "33";
+         dr1["UPD_IDNM"] = "44";
+         dr1["UPD_DATE"] = "55";
+         dr1["FLAG"] = "66";
+
+         dtParams.Rows.Add(dr1);
+
+         DataRow dr2 = dtParams.NewRow();
+
+         dr2["SEQ_NO"] = "a";
+         dr2["COMMON_TXT"] = "b";
+         dr2["USE_YN"] = "c";
+         dr2["UPD_IDNM"] = "d";
+         dr2["UPD_DATE"] = "e";
+         dr2["FLAG"] = "f";
+
+         dtParams.Rows.Add(dr2);
+
+         DataRow dr3 = dtParams.NewRow();
+
+         dr3["SEQ_NO"] = "가";
+         dr3["COMMON_TXT"] = "나";
+         dr3["USE_YN"] = "다";
+         dr3["UPD_IDNM"] = "라";
+         dr3["UPD_DATE"] = "마";
+         dr3["FLAG"] = "바";
+
+         dtParams.Rows.Add(dr3);
+
+
+         //dtParams.Rows.Add(dr1);
+
+         rptList.DataSource = dtParams;
+         rptList.DataBind();
+      }
+
+      protected void addNewRow(object sender, EventArgs e)
+      {
+         addRow();
+      }
+
+      protected void rowUp(object sender, RepeaterItemEventArgs e)
+      {
+         int k = e.Item.ItemIndex;
+         DataTable dtParams = new DataTable();
+         dtParams.Columns.Add(new DataColumn("SEQ_NO", typeof(String)));
+         dtParams.Columns.Add(new DataColumn("COMMON_TXT", typeof(String)));
+         dtParams.Columns.Add(new DataColumn("USE_YN", typeof(String)));
+         dtParams.Columns.Add(new DataColumn("UPD_IDNM", typeof(String)));
+         dtParams.Columns.Add(new DataColumn("UPD_DATE", typeof(String)));
+         dtParams.Columns.Add(new DataColumn("FLAG", typeof(String)));
+      }
+
+      protected void rptItemCommand(object source, RepeaterCommandEventArgs e)
+      {
+
+        
+         //순번 up
+         if (e.CommandName == "up_Click")
+         {
+            int k = e.Item.ItemIndex;
+            Response.Write(k);
+
+            //DataTable dtParams = new DataTable();
+            //dtParams.Columns.Add(new DataColumn("SEQ_NO", typeof(String)));
+            //dtParams.Columns.Add(new DataColumn("COMMON_TXT", typeof(String)));
+            //dtParams.Columns.Add(new DataColumn("USE_YN", typeof(String)));
+            //dtParams.Columns.Add(new DataColumn("UPD_IDNM", typeof(String)));
+            //dtParams.Columns.Add(new DataColumn("UPD_DATE", typeof(String)));
+            //dtParams.Columns.Add(new DataColumn("FLAG", typeof(String)));
+            //DataRow dr = dtParams.NewRow();
+
+
+
+
+
+         }
+      }
+      
+
+
+
+      private void InitializeComponent()
+      {
+         this.Load += new System.EventHandler(this.Page_Load);
+         this.btnAdd.Click += new System.EventHandler(this.addNewRow);
+      }
+   }
+}
